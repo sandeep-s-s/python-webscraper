8849774efe873a521e3ff46abf1ac263474a18b5
Sandeep <sandeepsreekumar75@gmail.com>
2019-02-02 15:09:06 +0530

    comments added

--- web-scraper.py
+++ web-scraper.py
@@ -2,10 +2,9 @@ import requests
 import bs4
 
 res = requests.get('https://en.wikipedia.org/wiki/Artificial_intelligence')
-soup = bs4.BeautifulSoup(res.text,'lxml')
+soup = bs4.BeautifulSoup(res.text,'lxml') #parsing is done with with the help of lxml parser
 
 
-for x in soup.select('.mw-headline'):
+for x in soup.select('.mw-headline'): #iteration based on ".mw-headline"  class , we use id(#) also html tags
 	print(x.text)
 
-	
