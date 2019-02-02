aeddd57170efceee1b7e8ee906d36b0cb56b4b2d
Sandeep <sandeepsreekumar75@gmail.com>
2019-02-02 14:49:40 +0530

    simple /home/sandeep/Desktop/python-webscraper/.git

---
+++ web-scraper.py
@@ -0,0 +1,9 @@
+import requests
+import bs4
+
+res = requests.get('https://en.wikipedia.org/wiki/Artificial_intelligence')
+soup = bs4.BeautifulSoup(res.text,'lxml')
+
+
+for x in soup.select('.mw-headline'):
+	print(x.text)
